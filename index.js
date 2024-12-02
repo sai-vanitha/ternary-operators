73ec8658f5640b9fa46a97e65d52407501737ce9
vanitha <saivanitha21221@gmail.com>
2024-12-03 00:17:17 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,62 @@
+// console.log(true && false);
+
+// console.log(false || true);
+
+// const result = !true;
+// console.log(result);
+
+// console.log(10 > 5 && 3 < 4);
+
+// console.log(5 === 5 || 5 > 10);
+
+// const x = false;
+// const y = true;
+// console.log(x && y || !x);
+
+// console.log(!!(5 > 3));
+
+// // const res = false || 0 || "hello";
+// // console.log(res);
+
+// console.log(null && "JavaScript");
+
+// console.log(true || false && false);
+
+// console.log(!("hello" && 0));
+
+// console.log(10 || 0 && 5);
+
+// const res = "abc" && "def" || "";     
+// console.log(res);
+
+// console.log(3 > 2 && 2 > 4);
+
+// console.log(false || NaN || undefined);
+
+// var a="A";
+// var b="B";
+// var c="75";
+// var res = (a =="A" && b == "B" && c >=75)?"eligible":"not eligible";
+//  console.log(res);
+
+// var age="13";
+// var res=(age<"13")?"child":(age >= 13 && age <19)?
+// "teenager":"adult";
+// console.log(res);
+
+
+// var a ="session active is true";
+// var b ="welcome back";
+// var c ="please login";
+// var res = (a>"session active is true" && b>"welcome back" || c<="please login");
+// console.log(res);
+
+
+//  var score="69";
+ 
+//  var res=(score >="90")?"A":(score>= 80 && score<= 89)?"B":(score>=70 && score<=79)?"C":"FAIL";
+//  console.log(res);
+
+var price= 100;
+var res =(price > 100)? "the discount is 20%": "the disdount is 10%";
+console.log(res);
